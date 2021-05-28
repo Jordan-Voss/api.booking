f468434b67b61c401db2cb1caf82f33f9d9aa463
Jordan-Voss <jordan.voss2@mail.dcu.ie>
2021-05-28 14:35:46 +0100

    add cors

--- src/main/java/tutorapp/api/HelloWorldController.java
+++ src/main/java/tutorapp/api/HelloWorldController.java
@@ -3,6 +3,7 @@ package tutorapp.api;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,6 +15,7 @@ public class HelloWorldController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @CrossOrigin(origins = "http://localhost:8100")
     @GetMapping("/hello-world")
     @ResponseBody
     public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="World") String name) {
