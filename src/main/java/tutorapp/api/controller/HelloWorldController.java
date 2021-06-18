f368b8db550396d04d257abf6675c43439d372ee
Jordan-Voss <jordan.voss2@mail.dcu.ie>
2021-06-18 02:30:41 +0100

    add back hello world controller and service

--- src/main/java/tutorapp/api/controller/HelloWorldController.java
+++ src/main/java/tutorapp/api/controller/HelloWorldController.java
@@ -1,25 +1,25 @@
-//package tutorapp.api.controller;
-//
-//import java.util.concurrent.atomic.AtomicLong;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//import tutorapp.api.service.Greeting;
-//
-//@Controller
-//public class HelloWorldController {
-//
-//    private static final String template = "Hello, %s!";
-//    private final AtomicLong counter = new AtomicLong();
-//
-//    @CrossOrigin(origins = "*")
-//    @GetMapping("/hello-world")
-//    @ResponseBody
-//    public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="World") String name) {
-//        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-//    }
-//
-//}
+package tutorapp.api.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import tutorapp.api.service.Greeting;
+
+@Controller
+public class HelloWorldController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/hello-world")
+    @ResponseBody
+    public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
--- src/main/java/tutorapp/api/service/Greeting.java
+++ src/main/java/tutorapp/api/service/Greeting.java
@@ -1,25 +1,25 @@
-//package tutorapp.api.service;
-//
-//public class Greeting {
-//
-//    private final long id;
-//    private final String content;
-//
-//    public Greeting() {
-//        this.id = -1;
-//        this.content = "";
-//    }
-//
-//    public Greeting(long id, String content) {
-//        this.id = id;
-//        this.content = content;
-//    }
-//
-//    public long getId() {
-//        return id;
-//    }
-//
-//    public String getContent() {
-//        return content;
-//    }
-//}
+package tutorapp.api.service;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting() {
+        this.id = -1;
+        this.content = "";
+    }
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
